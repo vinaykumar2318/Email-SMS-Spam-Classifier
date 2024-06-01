5b55acbbe70940e625b4b5167e31e9aacc33b7bd
Vinay Kumar <138013792+vinaykumar2318@users.noreply.github.com>
2024-06-01 14:39:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle
+from nltk.corpus import stopwords
+import string
+import nltk
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+def transformText(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    newText = []
+    for i in text:
+        if i.isalnum():
+            newText.append(i)
+
+    text = newText[:]
+    newText.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            newText.append(ps.stem(i))
+    return " ".join(newText)
+
+td = pickle.load(open('vectorizer.pkl','rb'))
+mnb = pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+#Preprocess
+inputText = st.text_area("Enter the Message")
+if st.button("Predict"):
+    transText = transformText(inputText)
+
+    #Vectorization
+    vector = td.transform([transText])
+
+    #Prediction
+    ans = mnb.predict(vector)[0]
+
+    if ans==1:
+        st.header("SPAM")
+    else:
+        st.header("NOT SPAM")
